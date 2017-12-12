0bf41d8509deed7a628f192e025d3952dc1d5e3a
alban porcheron <alban.porcheron@gmail.com>
2017-12-12 12:20:40 +0100

    Content for sections of landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about' => 'pages#about'
+  get 'jobs' => 'pages#jobs'
+  get 'access' => 'pages#access'
 end
