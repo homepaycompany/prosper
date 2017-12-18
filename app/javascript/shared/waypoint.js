e5c64aeff86a42672cf43f67c5a2b0b120d4d339
alban porcheron <alban.porcheron@gmail.com>
2017-12-18 11:00:47 +0100

    Added animate css and waypoint on landing page

--- app/javascript/shared/waypoint.js
+++ app/javascript/shared/waypoint.js
@@ -1,10 +1,15 @@
 import waypoints from 'waypoints/lib/noframework.waypoints.js';
 
+function addAnimation(item) {
+  item.classList.remove('hidden');
+  item.classList.add('animated', 'fadeIn');
+}
+
 const productsWaypoint = new Waypoint({
-  element: document.querySelector('.landing-products'),
+  element: document.querySelector('.landing-studio'),
   handler: function() {
-    document.querySelector('.landing-products img').classList.remove('hidden');
-    document.querySelector('.landing-products img').classList.add('animated', 'slideInRight');
+    const images = Array.from(document.querySelectorAll('.landing-products img'));
+    images.forEach(image => addAnimation(image));
   }
 })
 
