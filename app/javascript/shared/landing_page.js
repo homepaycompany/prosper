5135487a77bae78246ea98539f84556db26c0c49
alban porcheron <alban.porcheron@gmail.com>
2017-12-18 12:02:53 +0100

    Added animations on landing page

--- app/javascript/shared/landing_page.js
+++ app/javascript/shared/landing_page.js
@@ -11,7 +11,26 @@ function fadeInLeft(item) {
   item.classList.add('animated', 'fadeInLeft');
 }
 
+function fadeInRight(item) {
+  item.classList.remove('hidden');
+  item.classList.add('animated', 'fadeInRight');
+}
+
+function zoomIn(item) {
+  item.classList.remove('hidden');
+  item.classList.add('animated', 'zoomIn');
+}
+
 // Javascript to launch animation effects when scrolling to identified sections
+const realEstateWaypoint = new Waypoint({
+  element: document.querySelector('.landing-real-estate'),
+  handler: function() {
+    const image = document.querySelector('#landing-real-estate-media img');
+    fadeInRight(image);
+  },
+  offset: 200
+})
+
 const studioWaypoint = new Waypoint({
   element: document.querySelector('.landing-studio'),
   handler: function() {
@@ -28,4 +47,13 @@ const productsWaypoint = new Waypoint({
     images.forEach(image => fadeIn(image));
   },
   offset: 200
+})
+
+const teamWaypoint = new Waypoint({
+  element: document.querySelector('.landing-team'),
+  handler: function() {
+    const images = Array.from(document.querySelectorAll('#landing-team-media img'));
+    images.forEach(image => fadeIn(image));
+  },
+  offset: 400
 })
