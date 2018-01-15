7d6bc807b5bbab4f68fd3b9efb70aff4883cb28d
alban porcheron <alban.porcheron@gmail.com>
2018-01-15 10:02:28 +0100

    Added sections on static pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
   get 'access' => 'pages#access'
   get 'contact' => 'pages#contact'
-  get 'jobs' => 'pages#jobs'
   post :subscribe, controller: :subscriptions, action: :subscribe
   post :access_request, controller: :accesses, action: :access_request
 
