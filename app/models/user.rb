70f7f674d66278df4ddf619a87187a615317a0d8
alban porcheron <alban.porcheron@gmail.com>
2018-01-15 13:54:30 +0100

    Devise not registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :invitable, :invite_for => 2.weeks
+  # :confirmable, :lockable, :timeoutable, :omniauthable and :registerable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :invitable, :invite_for => 2.weeks
 
   has_many :positions, dependent: :destroy
 end
