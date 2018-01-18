ee73229b74e66b0aaa370ba43b4387b4fdee7b9c
alban porcheron <alban.porcheron@gmail.com>
2018-01-18 16:02:01 +0100

    Added a method to calculate the return rate of each property investment

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -16,4 +16,12 @@ class Flat < ApplicationRecord
   def price_per_squared_meter
     self.price / self.size
   end
+
+  def return_rate
+    @selling_price = (self.size.to_f * self.average_price.to_f)
+    @notarial_costs = (0.025 * @selling_price)
+    @interests = (self.size.to_f * 0.05 * 1)
+    @margin = @selling_price - self.price - @notarial_costs - @interests
+    return (@margin.to_f / self.price)
+  end
 end
