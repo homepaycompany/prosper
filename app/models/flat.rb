3ed728434e9e38a54b0c609119297b850a87f2bb
alban porcheron <alban.porcheron@gmail.com>
2018-01-18 16:41:09 +0100

    Added conditional style depending on investment return

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -20,7 +20,7 @@ class Flat < ApplicationRecord
   def return_rate
     @selling_price = (self.size.to_f * self.average_price.to_f)
     @notarial_costs = (0.025 * @selling_price)
-    @interests = (self.size.to_f * 0.05 * 1)
+    @interests = (self.size.to_f * 0.025 * 1)
     @margin = @selling_price - self.price - @notarial_costs - @interests
     return (@margin.to_f / self.price)
   end
