929cf3842ad32afa93ce9c88a6428b43f1c4e0c3
alban porcheron <alban.porcheron@gmail.com>
2018-01-18 12:24:08 +0100

    Added wish feature

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -6,13 +6,16 @@ class WishesController < ApplicationController
   def create
     @flat = Flat.find(params[:flat_id])
     @wish = Wish.new(user: current_user, flat: @flat)
+    authorize @wish
     @wish.save
+    redirect_back fallback_location: root_path
   end
 
   def destroy
     @wish = Wish.find(params[:id])
+    authorize @wish
     @wish.destroy
-    redirect_to wishes_path
+    redirect_back fallback_location: root_path
   end
 end
 
