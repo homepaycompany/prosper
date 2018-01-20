989dcbc549098ab103cbf11fa6e5046ce5e75701
alban porcheron <alban.porcheron@gmail.com>
2018-01-20 22:29:47 +0100

    Added geocoding

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,7 +2,15 @@ class FlatsController < ApplicationController
   before_action :set_flat, only: [:show]
 
   def index
-    @flats = policy_scope(Flat.all)
+    @flats = policy_scope(Flat.all).where.not(latitude: nil, longitude: nil)
+
+    @markers = @flats.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude,
+        infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat.title }) }
+      }
+    end
   end
 
   def show
