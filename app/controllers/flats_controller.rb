0f505e9f2de0f9414d78ef54e7883f7ec7b246e5
alban porcheron <alban.porcheron@gmail.com>
2018-01-22 19:17:42 +0100

    Added web service to Flat index

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,7 +1,19 @@
+require 'net/http'
+require 'uri'
+
 class FlatsController < ApplicationController
   before_action :set_flat, only: [:show]
 
   def index
+    uri = URI('https://testprop.azurewebsites.net/api/LeboncoinApi?code=3O49D3iZBLQvVAhpRq/UYbFBjGWVvaRzwYp94rkSljL4TjpCwVDu4w==')
+    Net::HTTP.start(uri.host, uri.port,
+      :use_ssl => uri.scheme == 'https') do |http|
+      req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+      req.body = { "City": "Paris", "ZipCode": "75001", "DateFrom": "0001-01-01T00:00:00", "DateTo": "9999-12-31T23:59:59.9999999" }.to_json
+      res = http.request req
+      @flats = JSON.parse(res.body)
+    end
+
     @flats = policy_scope(Flat.all).where.not(latitude: nil, longitude: nil)
 
     @markers = @flats.map do |flat|
