4aec8035e1c9f512b7498df7c842c06f7c98bd0b
alban porcheron <alban.porcheron@gmail.com>
2018-02-05 09:55:05 +0100

    Setup of API V2 for wishes index

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,5 +1,7 @@
 class WishesController < ApplicationController
   before_action :set_flats, only: [:index]
+  before_action :set_flats, :sort_flats, :filter_flats, only: [:index]
+  before_action :clean_params, only: [:index]
 
   def index
     # Select flats which have been added to the wishlist
@@ -44,19 +46,69 @@ class WishesController < ApplicationController
   private
 
   def set_flats
-    uri = URI("https://propertyhubprod.azurewebsites.net/api/RestApi?code=#{ENV['PROPERTY_HUB_API_KEY']}")
+    uri = URI("https://propertyhubstaging.azurewebsites.net/api/JsonApi?code=#{ENV['PROPERTY_HUB_API_KEY']}")
     Net::HTTP.start(uri.host, uri.port,
       :use_ssl => uri.scheme == 'https') do |http|
       req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
       req.body = { "City": current_user.city,"ZipCode": current_user.zip_code,"DateFrom":"0001-01-01T00:00:00","DateTo":"9999-12-31T23:59:59.9999999" }.to_json
       res = http.request req
-      @flats = JSON.parse(res.body).sort_by { |flat| -DateTime.strptime(flat["date"]).to_f }
-      # Add an id to the different flats obtained from the API
-      i = 0
-      @flats.map do |flat|
-        i += 1
-        flat["id"] = i
-      end
+      @flats = JSON.parse(res.body)["bids"]
     end
+  end
+
+  # Sort flats according to the criteria chosen by the visitor
+  def sort_flats
+    @sort_type = params["sort"]
+    if @sort_type == "a-price"
+      @flats.sort_by! { |flat| flat["price"].to_f }
+    elsif @sort_type == "d-price"
+      @flats.sort_by! { |flat| -flat["price"].to_f }
+    elsif @sort_type == "a-surface"
+      @flats.sort_by! { |flat| flat["surface"].to_f }
+    elsif @sort_type == "d-surface"
+      @flats.sort_by! { |flat| -flat["surface"].to_f }
+    elsif @sort_type == "a-return"
+      @flats.sort_by! { |flat| -DateTime.strptime(flat["date"]).to_f }
+    elsif @sort_type == "d-return"
+      @flats.sort_by! { |flat| -DateTime.strptime(flat["date"]).to_f }
+    elsif @sort_type == "a-date"
+      @flats.sort_by! { |flat| DateTime.strptime(flat["date"]).to_f }
+    else
+      @flats.sort_by! { |flat| -DateTime.strptime(flat["date"]).to_f }
+    end
+  end
+
+  # Filter flats according to the criteria chosen by the visitor
+  def filter_flats
+    @price_min = params['price_min'].to_i
+    @price_max = params['price_max'].to_i
+    @return_min = params['return_min'].to_i / 100
+    @return_max = params['return_max'].to_i / 100
+    @surface_min = params['surface_min'].to_i
+    @surface_max = params['surface_max'].to_i
+    if params['room_5'] == 1
+      @room_nb = 5
+    elsif params['room_4'] == 1
+      @room_nb = 4
+    elsif params['room_3'] == 1
+      @room_nb = 3
+    elsif params['room_2'] == 1
+      @room_nb = 2
+    else
+      @room_nb = 0
+    end
+
+    @flats.select!{|flat| flat['price'] >= @price_min if flat['price']} if @price_min > 0
+    @flats.select!{|flat| flat['price'] <= @price_max if flat['price']} if @price_max > 0
+    # @flats.select!{|flat| flat['return'] >= @return_min if flat['return']} if @return_min > 0
+    # @flats.select!{|flat| flat['return'] <= @return_max if flat['return']} if @return_max > 0
+    @flats.select!{|flat| flat['surface'] >= @surface_min if flat['surface']} if @surface_min > 0
+    @flats.select!{|flat| flat['surface'] <= @surface_max if flat['surface']} if @surface_max > 0
+    @flats.select!{|flat| flat['rooms'] >= @room_nb if flat['rooms']} if @room_nb > 1
+  end
+
+  # Clean unused filters
+  def clean_params
+    params.reject{|_,v| v == ""}
   end
 end
