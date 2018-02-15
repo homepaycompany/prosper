c00a20fbc99cef33bfddc91608136750772d27cc
alban porcheron <alban.porcheron@gmail.com>
2018-02-15 10:01:23 +0100

    Destroy city accesses dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :omniauthable and :registerable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :invitable, :invite_for => 2.weeks
 
   has_many :wishes, dependent: :destroy
