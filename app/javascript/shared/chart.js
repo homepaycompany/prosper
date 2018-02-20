55adb5984e379cb3c1b93bb057c163c6ab73a578
alban porcheron <alban.porcheron@gmail.com>
2018-02-20 11:03:20 +0100

    Generate chart only if the flat has a surface

--- app/javascript/shared/chart.js
+++ app/javascript/shared/chart.js
@@ -1,39 +1,42 @@
 function addChart() {
-  new Chartkick.ScatterChart(
-    "flat-chart",
-    data,
-    { legend: false,
-      xtitle: "Prix",
-      ytitle: "Surface",
-      library: {
-        tooltip: {
-          enabled: true,
-          useHTML: true,
-          style: { pointerEvents: 'auto' },
-          formatter: function() {
-            return `<a href="${this.series.name}">Voir l'annonce</a>`;
+  if (document.querySelector('#flat-chart')) {
+    new Chartkick.ScatterChart(
+      "flat-chart",
+      data,
+      { legend: false,
+        xtitle: "Prix",
+        ytitle: "Surface",
+        library: {
+          tooltip: {
+            enabled: true,
+            useHTML: true,
+            style: { pointerEvents: 'auto' },
+            formatter: function() {
+              return `<a href="${this.series.name}">Voir l'annonce</a>`;
           }
         }
       }
     });
-  new Chartkick.ScatterChart(
-    "flat-chart-size",
-    data2,
-    { legend: false,
-      xtitle: "Prix",
-      ytitle: "Surface",
-      library: {
-        tooltip: {
-          enabled: true,
-          useHTML: true,
-          style: { pointerEvents: 'auto' },
-          formatter: function() {
-            return `<a href="${this.series.name}">Voir l'annonce</a>`;
+  }
+  if (document.querySelector('#flat-chart-size')) {
+    new Chartkick.ScatterChart(
+      "flat-chart-size",
+      data2,
+      { legend: false,
+        xtitle: "Prix",
+        ytitle: "Surface",
+        library: {
+          tooltip: {
+            enabled: true,
+            useHTML: true,
+            style: { pointerEvents: 'auto' },
+            formatter: function() {
+              return `<a href="${this.series.name}">Voir l'annonce</a>`;
           }
         }
       }
     });
-  // new Chartkick.ScatterChart("flat-chart-size", data2, {legend: false, colors: ["#243059", "#20E8B6"], xtitle: "Prix/m²", ytitle: "Surface"});
+  }
 }
 
 export { addChart };
