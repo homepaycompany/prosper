1c734afa573fa5d307ee1811d75f2b541a2dd909
alban porcheron <alban.porcheron@gmail.com>
2018-02-28 15:24:12 +0100

    Added minimum on chart

--- app/javascript/shared/chart.js
+++ app/javascript/shared/chart.js
@@ -1,4 +1,22 @@
+// Function to identify the minimum of an array
+Array.min = function( array ){
+  return Math.min.apply( Math, array );
+};
+
+// Minimum value to show on the chart
+const minValues = data.map(function(item) {
+                    return item.data[0][1];
+                  });
+const minY = Array.min(minValues);
+const minValues2 = data2.map(function(item) {
+                    return item.data[0][1];
+                  });
+const minY2 = Array.min(minValues2);
+
+// Function to add charts on Flat#show
 function addChart() {
+  console.log(minValues);
+  console.log(minY);
   new Chartkick.ScatterChart(
     "flat-chart",
     data,
@@ -6,6 +24,9 @@ function addChart() {
       xtitle: "Prix (€)",
       ytitle: "Surface (m²)",
       library: {
+        yAxis: {
+          min: minY
+        },
         tooltip: {
           enabled: true,
           useHTML: true,
@@ -23,6 +44,9 @@ function addChart() {
       xtitle: "Prix (€/m²)",
       ytitle: "Surface (m²)",
       library: {
+        yAxis: {
+          min: minY2
+        },
         tooltip: {
           enabled: true,
           useHTML: true,
