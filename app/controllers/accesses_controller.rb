0669a385fddf28adaddd2fd4ccb872e50a4dec8d
alban porcheron <alban.porcheron@gmail.com>
2018-05-13 21:30:30 +0200

    Rectified access redirection

--- app/controllers/accesses_controller.rb
+++ app/controllers/accesses_controller.rb
@@ -2,20 +2,18 @@ class AccessesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:access_request]
 
   def access_request
-    @subscription = Access.new(access_request_params)
-    authorize @subscription
-    if @subscription.save
-      redirect_to root_path, :flash => { :notice => "We registered your access request for #{@subscription.email}. We will get back to you very soon." }
-    elsif @subscription.errors.messages[:email][0] == "is invalid"
-      redirect_to access_path, :flash => { :alert => "Invalid email format." }
+    @access = Access.new(access_request_params)
+    authorize @access
+    if @access.save
+      redirect_to root_path, :flash => { :notice => "Vous êtes bien enregistrés sur notre liste d'attente. Nous vous enverons rapidement un email à #{@access.email}." }
     else
-      redirect_to access_path, :flash => { :alert => "We encountered a problem to proceed your access request." }
+      redirect_to access_path, :flash => { :alert => "Nous avons rencontré un problème lors de votre inscription." }
     end
   end
 
   private
 
   def access_request_params
-    params.permit(:email)
+    params.permit(:first_name, :last_name, :email)
   end
 end
